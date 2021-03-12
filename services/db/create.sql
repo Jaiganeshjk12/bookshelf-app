df6536e7662a1f241a517b44f58c56f501ee841c
jaiganesh <jaiganesh@kasten.io>
2021-03-12 12:28:41 +0530

    Added initdb script

--- services/db/create.sql
+++ services/db/create.sql
@@ -1,1 +1,11 @@
 CREATE DATABASE books;
+CREATE TABLE BOOKS(
+ID INT NOT NULL,
+TITLE  VARCHAR    NOT NULL,
+AUTHOR VARCHAR    NOT NULL,
+READ   BOOL NOT NULL
+);
+
+INSERT INTO BOOKS (ID,TITLE,AUTHOR,READ) VALUES (1, 'On the Road', 'Jack Kerouac', true);
+INSERT INTO BOOKS (ID,TITLE,AUTHOR,READ) VALUES (2, 'Harry Potter and the Philosophers Stone', 'J. K. Rowling', false);
+INSERT INTO BOOKS (ID,TITLE,AUTHOR,READ) VALUES (3, 'Green Eggs and Ham', 'Dr. Suess', true);
